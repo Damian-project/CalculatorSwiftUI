ecd6f1c04f8777a46b08c61962ad5ed6db1a0519
Damian <damian.bartnowski@gmail.com>
2021-10-02 16:21:14 +0200

    Almost completed UI without funcionality

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -7,11 +7,97 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case substract = "-"
+    case multiply = "x"
+    case devide = "/"
+    case equal = "="
+    case clear = "AC"
+    case decimal = ","
+    case percent = "%"
+    case negative = "+/-"
+    
+    var buttonColor: Color {
+        switch self {
+        case .devide, .multiply, .substract, .add, .equal:
+            return .orange
+        case .clear, .negative, .percent:
+            return .gray
+        default:
+            return Color(UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1))
+        }
     }
+}
+
+struct ContentView: View {
+    
+    let buttons: [[CalcButton]] = [
+        [.clear, .negative, .percent, .devide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .substract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal]
+    ]
+    
+    var body: some View {
+        ZStack{
+            Color.black.ignoresSafeArea()
+            
+            VStack{
+                //MARK:- result text
+                Spacer()
+                HStack{
+                    Spacer()
+                    Text("0")
+                        .bold()
+                        .font(.system(size: 64))
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+                    ForEach(buttons, id: \.self) { row in
+                        HStack(spacing: 12) {
+                            ForEach(row, id: \.self) { item in
+                                Button(action: {
+                                    
+                                }, label: {
+                                    Text(item.rawValue)
+                                        .bold()
+                                        .font(.system(size: 32))
+                                        .frame(
+                                            width: self.buttonWidth(item: item),
+                                            height: self.buttonHeight())
+                                        .background(item.buttonColor)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(buttonHeight() / 2 )
+                                })
+                            }
+                        }
+                        .padding(.bottom, 3)
+                }
+            }
+        }
+    }
+    
+    func buttonWidth(item: CalcButton) -> CGFloat {
+        return (UIScreen.main.bounds.width - (5*12)) / 4
+    }
+    
+    func buttonHeight() -> CGFloat {
+        return (UIScreen.main.bounds.width - (5*12)) / 4
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
